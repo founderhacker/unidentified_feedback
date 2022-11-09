1d6db18861d3a89bf5fdc61cc0642aecbbf97845
Ryan Kulp <ryanckulp@gmail.com>
2022-11-09 09:45:32 -0500

    create Feedback model for db interactions

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def bark!
+    puts "bark!"
+  end
 end
---
+++ app/models/feedback.rb
@@ -0,0 +1,13 @@
+class Feedback < ApplicationRecord
+
+  # pseudo-code logic
+  # 1. user creates feedback record
+  # 2. tweet the "text" attribute of the feedback record, to the "recipient_handle" attribute
+
+  after_create :send_tweet
+
+  def send_tweet
+    puts "Sending tweet!"
+    # insert twitter API logic here
+  end
+end
