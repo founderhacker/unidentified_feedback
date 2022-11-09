ddd445e588189939d7f07739db706a7cec7ddf0a
Ryan Kulp <ryanckulp@gmail.com>
2022-11-09 16:56:41 -0500

    convert frontend form to "rails way" and handle request params on backend

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,13 +1,7 @@
 class Feedback < ApplicationRecord
-
-  # pseudo-code logic
-  # 1. user creates feedback record
-  # 2. tweet the "text" attribute of the feedback record, to the "recipient_handle" attribute
-
   after_create :send_tweet
 
   def send_tweet
-    puts "Sending tweet!"
-    # insert twitter API logic here
+    TwitterService.tweet!(text)
   end
 end
