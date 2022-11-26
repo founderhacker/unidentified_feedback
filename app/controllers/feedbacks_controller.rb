6d3a78d54baf28532dbee5023b56bb82a1ff84dd
Elman Mansimov <elman.mansimov@gmail.com>
2022-11-26 18:49:56 -0500

    ability to view existing feedback on the frontend

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -6,6 +6,20 @@ class FeedbacksController < ApplicationController
     redirect_to root_path
   end
 
+  def exists
+    # function that checks if the handle exists in the database
+    # then returns a json object with true
+
+    handle = params[:handle]
+    exists = Feedback.exists?(recipient_handle: handle)
+    if exists
+      feedback = Feedback.where(recipient_handle: handle).last
+      render json: {exists: true, existing_feedback_tweet_url: feedback.tweet_url}
+    else
+      render json: {exists: false}
+    end
+  end
+
   private
 
   def feedback_params
