329b2852785298f2dd71952fb7f23d83f4b56d7d
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-22 00:19:17 -0500

    on stripe payment link redirect to /thanks, send email to me with tweet_url to pin

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,7 @@ class AdminMailer < ApplicationMailer
   default from: ENV['admin_email']
 
   def pinned_tweet_purchased_email
+    @url = Feedback.last[:tweet_url]
     mail(to: ENV['admin_email'], subject: 'A pinned tweet has been purchased')
   end
 end
