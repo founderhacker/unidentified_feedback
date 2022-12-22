472aacca69539156ce1509b6da4f45b6ff9ac673
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-22 00:19:41 -0500

    on stripe payment link redirect to /thanks, send email to me with tweet_url to pin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def thanks
+    # when user gets to this page, send email
+    AdminMailer.pinned_tweet_purchased_email.deliver_later
+  end
+  
 end
