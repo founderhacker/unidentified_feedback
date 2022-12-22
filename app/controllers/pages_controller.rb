a43081a56ba03f4a2ac82c6a4a355c7e8ec6de00
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-22 18:08:09 -0500

    cleaned up stripe service + passed feedback_id from frontend to backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @pay_link_url = StripeService.create_pay_link(session[:feedback_id])
   end
 
   def thanks
+    @feeback_id = params[:feeback_id]
     # when user gets to this page, send email to me
-    AdminMailer.pinned_tweet_purchased_email.deliver_later
+    AdminMailer.with(feeback_id: @feeback_id).pinned_tweet_purchased_email.deliver_later
   end
   
 end
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,7 +3,10 @@ class AdminMailer < ApplicationMailer
 
   # this mailer action sends the email
   def pinned_tweet_purchased_email
+    @feeback_id = params[:feeback_id]
     @url = Feedback.last[:tweet_url] # grab last Feedback
     mail(to: ENV['admin_email'], subject: 'A pinned tweet has been purchased') # send email
+
+    puts "YOOOOOOO #{Feedback.where(id: @feeback_id)}"
   end
 end
