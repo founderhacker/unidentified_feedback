1b00ba61f96677cba5af0fd8cedba30c96440338
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-26 14:59:33 -0500

    each feedback delayed by 1 minute between each other

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -16,8 +16,18 @@ class Feedback < ApplicationRecord
   end
 
   def send_tweet
-    #TwitterService.tweet!(self)
-    job = TwitterServiceJob.set(wait: ENV['delayed_delay_time'].to_i.minute).perform_later(self)
+
+    # check if there are jobs and check to see if they most recent job has happened already. if not, use that job's run_at time and add X minutes to it to create the run_at time for the next job to be run
+
+    # else just delay by X minutes (X is modifiable via ENV)
+    if Delayed::Job.count > 0 
+      if Delayed::Job.last.run_at.past? == false
+        next_scheduled_job_run_at_time = Delayed::Job.last.run_at + (ENV['delayed_delay_minutes'].to_i * 60)
+        job = TwitterServiceJob.set(wait_until: next_scheduled_job_run_at_time).perform_later(self)
+      end
+    else 
+      job = TwitterServiceJob.set(wait: ENV['delayed_delay_minutes'].to_i.minute).perform_later(self)
+    end
     self.update(delayed_job_id: JSON.parse(job.to_json)["provider_job_id"]) # update feedback with the id its job
     self.update(delayed_job_scheduled_at: JSON.parse(job.to_json)["scheduled_at"]) # update feedback with the id its job
   end
